d66dcb224a520ff87cec348d2f16f39d7d764750
Sam O <2274654+samogden@users.noreply.github.com>
2024-07-24 16:00:32 +0200

    Clearing out code for students

--- programming-assignments/PA2/src/student_code.c
+++ programming-assignments/PA2/src/student_code.c
@@ -5,55 +5,83 @@
 #include "student_code.h"
 #include "stdio.h"
 
-
 void mark_start(PROCESS* p, SCHEDULER_STATS* stats, float time_started) {
-  //todo
+  // todo
 }
 void mark_end(PROCESS* p, SCHEDULER_STATS* stats, float time_completed) {
-  //todo
+  // todo
 }
 
 void finalize_stats(SCHEDULER_STATS* stats) {
-  //todo
+  // todo
+}
+
+int compare_noop(PROCESS p1, PROCESS p2) {
+  return 0;
 }
 
 int compare_priority(PROCESS p1, PROCESS p2) {
-  //todo
-  return (int)p2.priority - (int)p1.priority;
+  // todo
+}
+
+int compare_arrival_time(PROCESS p1, PROCESS p2) {
+  // todo
+}
+
+int compare_arrival_time_invert(PROCESS p1, PROCESS p2) {
+  // todo
+}
+
+int compare_job_duration(PROCESS p1, PROCESS p2) {
+  // todo
+}
+
+int compare_time_remaining(PROCESS p1, PROCESS p2) {
+  // todo
+}
+
+int compare_last_run(PROCESS p1, PROCESS p2) {
+  // todo
 }
 
 PROCESS* fifo_process_selector(PROCESS_LIST* pl) {
-  PROCESS *p = NULL;
-  //todo
+  PROCESS *p;
+  // todo: what should be this next line?
+  p = get_minimum(pl, compare_noop);
   return p;
 }
 
 PROCESS* rr_process_selector(PROCESS_LIST* pl) {
-  PROCESS *p = NULL;
-  //todo
+  PROCESS *p;
+  // todo: what should be this next line?
+  p = get_minimum(pl, compare_noop);
   return p;
 }
 
 PROCESS* sjf_process_selector(PROCESS_LIST* pl) {
-  PROCESS *p = NULL;
-  //todo
+  PROCESS *p;
+  // todo: what should be this next line?
+  p = get_minimum(pl, compare_noop);
   return p;
 }
 
 PROCESS* priority_process_selector(PROCESS_LIST* pl) {
-  PROCESS *p = NULL;
-  p = get_minimum(pl, compare_priority);
+  PROCESS *p;
+  // todo: what should be this next line?
+  p = get_minimum(pl, compare_noop);
   return p;
 }
 
 PROCESS* lifo_process_selector(PROCESS_LIST* pl) {
-  PROCESS *p = NULL;
-  //todo
+  PROCESS *p;
+  // todo: what should be this next line?
+  p = get_minimum(pl, compare_noop);
   return p;
 }
 
 PROCESS* stcf_process_selector(PROCESS_LIST* pl) {
-  PROCESS *p = NULL;
-  //todo
+  PROCESS *p;
+  // todo: what should be this next line?
+  p = get_minimum(pl, compare_noop);
   return p;
 }
