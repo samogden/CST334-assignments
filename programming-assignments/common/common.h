82fcfc9352e07f970c23b8d0f678233116d0807e
Sam O <2274654+samogden@users.noreply.github.com>
2024-09-01 15:24:10 -0700

    Fixing a weird output bug

--- programming-assignments/common/common.h
+++ programming-assignments/common/common.h
@@ -7,7 +7,7 @@
 
 #define log_debug(...) fprintf(stderr, "DEBUG:"); fprintf(stderr, __VA_ARGS__); fflush(stderr);
 #define log_info(...)  fprintf(stderr, "INFO:");  fprintf(stderr, __VA_ARGS__); fflush(stderr);
-#define log_warn(...)  fprintf(stderr, "INFO:");  fprintf(stderr, __VA_ARGS__); fflush(stderr);
+#define log_warn(...)  fprintf(stderr, "WARN:");  fprintf(stderr, __VA_ARGS__); fflush(stderr);
 #define log_error(...) fprintf(stderr, "ERROR:"); fprintf(stderr, __VA_ARGS__); fflush(stderr);
 
 #endif //COMMON_H
